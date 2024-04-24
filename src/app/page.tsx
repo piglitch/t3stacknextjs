79349a7988a2fb677f6b520d32aeb8f3d3f3852d
Avi <avibanerjee2000@gmail.com>
2024-04-24 23:46:22 +0530

    database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import Image from "next/image";
 import Link from "next/link";
-
+import { db } from "~/server/db";
+import { v4 as uuidv4 } from 'uuid';
 const mockUrls = [
   "https://images.unsplash.com/photo-1543424165-dc50e710c481?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bXVzY2xlJTIwY2Fyc3xlbnwwfHwwfHx8MA%3D%3D",
   "https://images.unsplash.com/photo-1623550995311-252fe7df3817?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bXVzY2xlJTIwY2Fyc3xlbnwwfHwwfHx8MA%3D%3D",
@@ -13,13 +16,22 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main>
       <div className="flex flex-wrap gap-4 ml-6">
         {
+          posts.map((post) => 
+            (
+              <div key={uuidv4()}>{post.name}</div>
+            )
+          )
+        }
+        {
           [...mockImages, ...mockImages, ...mockImages].map((image) => (
-            <div key={image.id} className="w-48">
+            <div key={uuidv4()} className="w-48">
               <Image src={image.url} alt="carslol" width={480} height={80}/>
             </div>
           ))
