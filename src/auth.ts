817cff52a3691aad43fddf3e5789c9301a4f10c1
Avi <avibanerjee2000@gmail.com>
2024-05-10 01:56:48 +0530

    creds idk

--- src/auth.ts
+++ src/auth.ts
@@ -8,9 +8,9 @@ export const {handlers, auth, signOut} = NextAuth({
   // Configure providers here
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_PROD_ID!, 
-      clientSecret: process.env.GITHUB_PROD_SECRET!
+      clientId: 'Ov23li3sWXtMqbz8KZRi', 
+      clientSecret: '7fc31ef4df992680b41366c2fd2d06c3b51cb747'
     }),
-],
+  ],
   adapter: DrizzleAdapter(db),
 });
