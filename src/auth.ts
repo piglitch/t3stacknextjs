c4af9379c71bfc29679b56ef4c0b3919f294d021
Avi <avibanerjee2000@gmail.com>
2024-05-16 00:02:00 +0530

    added google provider

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from "next-auth/providers/credentials";
 import { db } from "~/server/db";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
@@ -11,6 +12,10 @@ export const {handlers, auth, signOut} = NextAuth({
       clientId: process.env.GITHUB_ID!, 
       clientSecret: process.env.GITHUB_SECRET!
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!
+    })
 ],
   adapter: DrizzleAdapter(db),
 });
